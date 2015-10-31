d19d3df1bb1855c57de194a714f665017ff6f095
Bykov Denis <stivenrain@gmail.com>
2015-10-31 12:27:47 +0300

    commit static version

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Отрисовка карты  
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: {lat: 59.936391, lng: 30.321088} 
+  });
+}  
+
